fad475ede49f02e413d6caab39fff2c51b480462
Fábio Caldas <fabio.caldas@gmail.com>
2024-09-29 11:32:29 -0300

    Create initial project with vite/react/typescript #1

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <p>
+          Blackjack
+        </p>
+      </header>
+    </>
+  )
+}
+
+export default App
